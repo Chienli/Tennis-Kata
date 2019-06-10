51b9b9b58ce77c6a8c82f5002ca10a413df097ef
Guy Lin Chien Li <guy.lin@titansoft.com.sg>
2019-06-10 22:17:36 +0800

    love all pass

--- Tennis Kata/TennisGameUnitTests.cs
+++ Tennis Kata/TennisGameUnitTests.cs
@@ -3,137 +3,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tennis_Kata
 {
-    [TestClass]
-    public class TennisGameUnitTest
+    public class TennisGame
     {
-        private readonly TennisGame _tennisGame = new TennisGame();
-
-        [TestMethod]
-        public void Deuce_one()
+        public string Score()
         {
-            _tennisGame.SetPlayerScore(3, 3);
-
-            Assert.AreEqual(GameState.Deuce.ToString(), _tennisGame.GetResult());
+            return "Love_All";
         }
+    }
 
-        [TestMethod]
-        public void Deuce_two()
-        {
-            _tennisGame.SetPlayerScore(4, 4);
-
-            Assert.AreEqual(GameState.Deuce.ToString(), _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void Fifteen_All()
-        {
-            _tennisGame.SetPlayerScore(1, 1);
-
-            Assert.AreEqual(GameState.FifteenAll.ToString(), _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void Fifteen_Love()
-        {
-            _tennisGame.SetPlayerScore(1, 0);
-
-            Assert.AreEqual(GameState.FifteenLove.ToString(), _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void FirstPlayer_Advance()
-        {
-            _tennisGame.SetPlayerScore(4, 3);
-
-            var expected = _tennisGame.FirstPlayer.Name + GameState.Advance.ToString();
-
-            Assert.AreEqual(expected, _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void FirstPlayer_Win()
-        {
-            _tennisGame.SetPlayerScore(5, 3);
-
-            var expected = _tennisGame.FirstPlayer.Name + GameState.Win.ToString();
-
-            Assert.AreEqual(expected, _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void Forty_Love()
-        {
-            _tennisGame.SetPlayerScore(3, 0);
-
-            Assert.AreEqual(GameState.FortyLove.ToString(), _tennisGame.GetResult());
-        }
+    [TestClass]
+    public class TennisGameUnitTests
+    {
+        private readonly TennisGame _tennis = new TennisGame();
 
         [TestMethod]
         public void Love_All()
         {
-            _tennisGame.SetPlayerScore(0, 0);
-
-            Assert.AreEqual(GameState.LoveAll.ToString(), _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void Love_Fifteen()
-        {
-            _tennisGame.SetPlayerScore(0, 1);
-
-            Assert.AreEqual(GameState.LoveFifTeen.ToString(), _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void Love_Forty()
-        {
-            _tennisGame.SetPlayerScore(0, 3);
-
-            Assert.AreEqual(GameState.LoveForty.ToString(), _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void Love_Thirty()
-        {
-            _tennisGame.SetPlayerScore(0, 2);
-
-            Assert.AreEqual(GameState.LoveThirty.ToString(), _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void SecondPlayer_Advance()
-        {
-            _tennisGame.SetPlayerScore(3, 4);
-
-            var expected = _tennisGame.SecondPlayer.Name + GameState.Advance.ToString();
-
-            Assert.AreEqual(expected, _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void SecondPlayer_Win()
-        {
-            _tennisGame.SetPlayerScore(3, 5);
-
-            var expected = _tennisGame.SecondPlayer.Name + GameState.Win.ToString();
-
-            Assert.AreEqual(expected, _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void Thirty_All()
-        {
-            _tennisGame.SetPlayerScore(2, 2);
-
-            Assert.AreEqual(GameState.ThirtyAll.ToString(), _tennisGame.GetResult());
-        }
-
-        [TestMethod]
-        public void Thirty_Love()
-        {
-            _tennisGame.SetPlayerScore(2, 0);
-
-            Assert.AreEqual(GameState.ThirtyLove.ToString(), _tennisGame.GetResult());
+            Assert.AreEqual("Love_All", _tennis.Score());
         }
     }
 }
